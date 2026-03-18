ab52cd9e9b5915aa23f10b4c712512153753cd1a
cse-chandan-verma <cse.chandan.verma@gmail.com>
2026-03-18 11:45:33 +0530

    Update Assessment - 5

--- MovieTicketBookingSystem/booking-service/src/main/java/com/assessment/demo/service/BookingService.java
+++ MovieTicketBookingSystem/booking-service/src/main/java/com/assessment/demo/service/BookingService.java
@@ -1,10 +1,12 @@
 package com.assessment.demo.service;
 
-import org.springframework.stereotype.Service;
-
 import com.assessment.demo.booking.model.Booking;
 import com.assessment.demo.booking.model.Movie;
 import com.assessment.demo.client.MovieClient;
+import com.assessment.demo.event.BookingEvent;
+import com.assessment.demo.messaging.BookingMessageProducer;
+
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,23 +16,36 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class BookingService {
 
     private final MovieClient movieClient;
+    private final BookingMessageProducer messageProducer;
     private final List<Booking> bookings = new ArrayList<>();
     private final AtomicInteger idCounter = new AtomicInteger(100);
 
-    public BookingService(MovieClient movieClient) {
+    public BookingService(MovieClient movieClient, BookingMessageProducer messageProducer) {
         this.movieClient = movieClient;
+        this.messageProducer = messageProducer;
     }
 
     public Booking bookTicket(int movieId, int tickets) {
         Movie movie = movieClient.getMovieById(movieId);
 
         if (movie == null) {
-            throw new RuntimeException("Movie service is unavailable or movie not found. Please try again later.");
+            throw new RuntimeException("Movie service unavailable or movie not found.");
         }
 
         double totalAmount = movie.getPrice() * tickets;
         Booking booking = new Booking(idCounter.incrementAndGet(), movieId, tickets, totalAmount);
         bookings.add(booking);
+
+        // Send event to RabbitMQ
+        BookingEvent event = new BookingEvent(
+                booking.getBookingId(),
+                movieId,
+                tickets,
+                totalAmount,
+                "CONFIRMED"
+        );
+        messageProducer.sendBookingEvent(event);
+
         return booking;
     }
 
